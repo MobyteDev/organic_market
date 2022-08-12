5d491b3d0d58286b2911c519e0d29e3d767986af
Михаил Карякин <mkariakin@sfedu.ru>
2022-08-12 11:51:26 +0300

    Changes in main reverted

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:organic_market/common/app_colors.dart';
-import 'package:organic_market/data/subcategory.dart';
-import 'package:organic_market/ui/categories_page/categories_page.dart';
 import 'package:organic_market/ui/main_screen/main_screen.dart';
-import 'package:organic_market/ui/pick_subcategory_page/pick_subcategory_page.dart';
 import 'package:organic_market/ui/product_page/product_page.dart';
-import 'package:organic_market/ui/subcategory_page/subcategory_page.dart';
 
-//import 'package:organic_market/utils/app_router.gr.dart';
+import 'package:organic_market/utils/app_router.gr.dart';
 
 
 void main() {
@@ -17,7 +13,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-  //final appRouter = AutoRouter();
+  final appRouter = AutoRouter();
 
   @override
   Widget build(BuildContext context) {
@@ -27,24 +23,7 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (_, __) {
 
-        //PickSubcategoryPage(subcategories: [Subcategory("Чай"), Subcategory("Чай"), Subcategory("Чай"), Subcategory("Чай")])
-        //CategoriesPage()
-        //MainScreen()
-        //SubcategoryPage()
-        return MaterialApp(home: SubcategoryPage(), theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(15),
-                bottomLeft: Radius.circular(15),
-              ),
-            ),
-            backgroundColor: AppColors.white,
-          ),
-          // fontFamily: "Jost",
-          primarySwatch: Colors.blue,
-        ),);/*MaterialApp.router(
+        return MaterialApp.router(
 
           title: 'Flutter Demo',
           theme: ThemeData(
@@ -65,7 +44,7 @@ class MyApp extends StatelessWidget {
           routerDelegate: appRouter.delegate(),
           routeInformationParser: appRouter.defaultRouteParser(),
           debugShowCheckedModeBanner: false,
-        );*/
+        );
       },
     );
   }
